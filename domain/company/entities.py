bc6357b97606062eb1aa967c25cc32629f8eb98b
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2024-04-17 22:30:07 +0500

    fix(domain, company): check name length instead of name

--- domain/company/entities.py
+++ domain/company/entities.py
@@ -3,12 +3,11 @@ from dataclasses import dataclass
 
 @dataclass
 class Company:
+    name: str
+    id: int | None = None
+
     NAME_MIN_LENGTH = 3
     NAME_MAX_LENGTH = 16
-
-    id: int | None = None
-    name: str
-
 
     def __post_init__(self):
         if not isinstance(self.id, int | None):
@@ -19,8 +18,10 @@ class Company:
     def _validate_name(self):
         if not isinstance(self.name, str):
             raise TypeError
+        
+        name_len = len(self.name)
         if (
-            self.name < self.NAME_MIN_LENGTH
-            or self.name > self.NAME_MAX_LENGTH
+            name_len < self.NAME_MIN_LENGTH
+            or name_len > self.NAME_MAX_LENGTH
         ):
             raise ValueError('company name\'s length must be between {NAME_MIN_LENGTH} and {NAME_MAX_LENGTH}')
