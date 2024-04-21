9bb6f82e30ff90e53d5e592be0ea8cbb195395e2
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2024-04-21 19:51:30 +0500

    feat(adapters, user): make user map

---
+++ adapters/user/map.py
@@ -0,0 +1,38 @@
+from domain.user.entities import User
+
+from adapters.company.map import CompanyMap
+from api.gql.user.schemas import UserSchema
+
+
+class UserMap:
+
+    @classmethod
+    def serialize_one(cls, user: User):
+        return {
+            "id": user.id,
+            "username": user.username,
+            "company_id": user.company_id,
+            "company": (
+                CompanyMap.serialize_one(user.company)
+                if user.company else None
+            ),
+        }
+
+    @classmethod
+    def serialize_many(cls, users: list[User]):
+        return [ 
+            UserMap.serialize_one(user) 
+            for user in users 
+        ]
+    
+    @classmethod
+    def to_gql_schema(cls, user: User):
+        return UserSchema(
+            id=user.id,
+            username=user.username,
+            company_id=user.company_id,
+            company=(
+                CompanyMap.to_gql_schema(user.company)
+                if user.company else None
+            )
+        )
