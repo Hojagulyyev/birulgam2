496073bc5ac35bff999959c37d1e85d02cb32d4d
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2024-05-10 09:29:23 +0500

    feat(map): make deal map to_gql_schema method

--- adapters/deal/map.py
+++ adapters/deal/map.py
@@ -1,16 +1,33 @@
 from domain.deal.entities import Deal
 
+from api.gql.deal.schemas import DealSchema
+
 
 class DealMap:
-
-    @classmethod
-    def serialize_one(cls, deal: Deal):
-        ...
-
-    @classmethod
-    def serialize_many(cls, deals: list[Deal]):
-        ...
     
     @classmethod
     def to_gql_schema(cls, deal: Deal):
-        ...
+        if deal.id is None:
+            raise TypeError
+        
+        return DealSchema(
+            id=deal.id,
+            company_id=deal.company_id,
+            store_id=deal.store_id,
+            user_id=deal.user_id,
+            seller_id=deal.seller_id,
+            buyer_id=deal.buyer_id,
+
+            total_amount=deal.total_amount,
+            remaining_amount_due=deal.remaining_amount_due,
+            type=deal.type,
+
+            installments=deal.installments,
+            installment_amount=deal.installment_amount,
+            installment_trifle=deal.installment_trifle,
+            installment_expiration_date=deal.installment_expiration_date,
+            created_at=deal.created_at,
+            last_paid_at=deal.last_paid_at,
+            closed_at=deal.closed_at,
+            note=deal.note,
+        )
