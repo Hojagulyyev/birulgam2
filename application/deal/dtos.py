270133d91cd75d2d65d6377fb1607082b4274156
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2024-05-10 09:14:33 +0500

    feat(usecase): make create deal usecase

---
+++ application/deal/dtos.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from datetime import date, datetime
+
+
+@dataclass
+class CreateDealUsecaseDto:
+    company_id: int
+    store_id: int
+    user_id: int
+    seller_id: int | None
+    buyer_id: int | None
+
+    total_amount: int
+    remaining_amount_due: int
+    type: str
+
+    installments: int = 0
+    installment_amount: int = 0
+    installment_trifle: int = 0
+    installment_expiration_date: date | None = None
+    created_at: datetime = datetime.now()
+    last_paid_at: datetime | None = None
+    closed_at: datetime | None = None
+    note: str | None = None
--- application/deal/usecases.py
+++ application/deal/usecases.py
@@ -1,6 +1,8 @@
 from domain.deal.entities import Deal, DealPage
 from domain.deal.interfaces import IDealRepository
 
+from .dtos import CreateDealUsecaseDto
+
 
 class GetDealsUsecase:
 
@@ -13,3 +15,35 @@ class GetDealsUsecase:
     async def execute(self) -> DealPage:
         deal_page = await self.deal_repo.list()
         return deal_page
+
+
+class CreateDealUsecase:
+
+    def __init__(
+        self, 
+        deal_repo: IDealRepository,
+    ):
+        self.deal_repo = deal_repo
+    
+    async def execute(self, dto: CreateDealUsecaseDto) -> Deal:
+        deal = Deal(
+            id=None,
+            company_id=dto.company_id,
+            store_id=dto.store_id,
+            user_id=dto.user_id,
+            seller_id=dto.seller_id,
+            buyer_id=dto.buyer_id,
+            total_amount=dto.total_amount,
+            remaining_amount_due=dto.remaining_amount_due,
+            type=dto.type,
+            installments=dto.installments,
+            installment_amount=dto.installment_amount,
+            installment_trifle=dto.installment_trifle,
+            installment_expiration_date=dto.installment_expiration_date,
+            created_at=dto.created_at,
+            last_paid_at=dto.last_paid_at,
+            closed_at=dto.closed_at,
+            note=dto.note,
+        )
+        created_deal = await self.deal_repo.save(deal)
+        return created_deal
