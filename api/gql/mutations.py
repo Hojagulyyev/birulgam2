8c661e6368a620e88f34929727bb981335447927
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2024-05-23 08:57:54 +0500

    refactor(gql): remove main nested mutations

--- api/gql/mutations.py
+++ api/gql/mutations.py
@@ -1,18 +1,24 @@
 import strawberry
 
-from .contact.mutations import ContactMutations
-from .deal.mutations import DealMutations
-from .payment.mutations import PaymentMutations
+from .contact.resolvers import (
+    create_contact_resolver,
+    create_contact_response,
+)
+from .deal.resolvers import (
+    create_deal_resolver,
+    create_deal_response,
+)
+from .payment.resolvers import (
+    create_payment_resolver,
+    create_payment_response,
+)
 
 
 @strawberry.type
 class Mutation:
-    contact_mutations: ContactMutations = strawberry.field(
-        resolver=ContactMutations,
-    )
-    deal_mutations: DealMutations = strawberry.field(
-        resolver=DealMutations,
-    )
-    payment_mutations: PaymentMutations = strawberry.field(
-        resolver=PaymentMutations,
-    )
+    create_contact: create_contact_response = strawberry.mutation(
+        resolver=create_contact_resolver)
+    create_deal: create_deal_response = strawberry.mutation(
+        resolver=create_deal_resolver)
+    create_payment: create_payment_response = strawberry.mutation(
+        resolver=create_payment_resolver)
